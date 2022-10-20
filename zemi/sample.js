482bd95ddbaf4d174a4575112cf5eff596f979f4
izuhara2 <izuhara0221@gmail.com>
2022-10-20 16:29:32 +0900

    main

--- zemi/sample.js
+++ zemi/sample.js
@@ -1,14 +1,17 @@
-function change_slider(){
+slider1.addEventListener('change',()=>{
     var processing = document.querySelector( '#slider1' ).value;
     var output1 =  document.querySelector( '#processing_time' );
-
-    var devi = document.querySelector( '#slider2' ).value;
-    var output2 = document.querySelector( '#deviation' );
-
-    var flow = document.querySelector( '#slider3' ).value;
-    var output3 = document.querySelector( '#flow' );
-
     output1.innerText = processing;
+});
+
+slider2.addEventListener('change',()=>{
+    var devi = document.querySelector( '#slider2' ).value;
+    var output2 =  document.querySelector( '#deviation' );
     output2.innerText = devi;
+});
+
+slider3.addEventListener('change',()=>{
+    var flow = document.querySelector( '#slider3' ).value;
+    var output3 =  document.querySelector( '#flow' );
     output3.innerText = flow;
-}
+});
